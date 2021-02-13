0677bc91ed49d9aa9f6ed2062a5c18cfce0c6b4b
michael beamer <beamermichael@gmail.com>
2021-02-13 19:10:31 +0100

    added modal/login, reworked slider

--- script.js
+++ script.js
@@ -10,6 +10,30 @@ const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slide");
 const btnLeft = document.querySelector(".slider__btn--left");
 const btnRight = document.querySelector(".slider__btn--right");
+const dotsContainer = document.querySelector(".dots");
+
+//Modal
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnShowModal = document.querySelector(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+
+function openModal() {
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+  modal.style.display = "none";
+}
+
+btnShowModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+// window.addEventListener("click", closeModal);
 
 // Tabs component
 
@@ -36,6 +60,8 @@ function tabsComponent() {
   });
 }
 
+// Slider component
+
 function sliderComponent() {
   let currentSlide = 0;
   let maxSlidesLength = slides.length;
@@ -46,10 +72,37 @@ function sliderComponent() {
 
   // Functions
 
+  function createDots() {
+    slides.forEach((s, index) => {
+      dotsContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot dots__dot--active" data-slide="${index}"></button>`
+      );
+    });
+  }
+
   function goToSlide(slide) {
     slides.forEach((s, index) => {
       s.style.transform = `translateX(${index - slide}00%)`;
     });
+  }
+
+  function changeActive(slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  }
+
+  function dotsSlideControl(event) {
+    if (event.target.classList.contains("dots__dot")) {
+      const slide = event.target.dataset.slide;
+      goToSlide(slide);
+      changeActive(slide);
+    }
   }
 
   function nextSlide() {
@@ -58,7 +111,9 @@ function sliderComponent() {
     } else {
       currentSlide++;
     }
+
     goToSlide(currentSlide);
+    changeActive(currentSlide);
   }
 
   function previousSlide() {
@@ -68,15 +123,33 @@ function sliderComponent() {
       currentSlide--;
     }
     goToSlide(currentSlide);
+    changeActive(currentSlide);
   }
+
+  function slideArrowControl(event) {
+    if (event.key === "ArrowRight") {
+      nextSlide();
+      changeActive(currentSlide);
+    }
+    if (event.key === "ArrowLeft") {
+      previousSlide();
+      changeActive(currentSlide);
+    }
+  }
+
+  // Calls
+  createDots();
+  changeActive(0);
+  goToSlide(0);
 
   // Event handlers
   btnRight.addEventListener("click", nextSlide);
   btnLeft.addEventListener("click", previousSlide);
-  document.addEventListener("keydown", function (event) {
-    console.log(event);
-  });
+  document.addEventListener("keydown", slideArrowControl);
+  dotsContainer.addEventListener("click", dotsSlideControl);
 }
+
+// Modal component
 
 // Initializes everything
 function init() {
@@ -86,5 +159,4 @@ function init() {
 
 init();
 
-// change project height to max-height 285px or try overflow hidden because on desktop its to big - my portfolio
 // delete min height from grid project cards - gets weird when over 1444p why ? google it
