001e7fc934b1d43680148ff8f98a647a99e55b38
michael beamer <beamermichael@gmail.com>
2021-02-14 15:15:02 +0100

    added login auth

--- script.js
+++ script.js
@@ -12,28 +12,80 @@ const btnLeft = document.querySelector(".slider__btn--left");
 const btnRight = document.querySelector(".slider__btn--right");
 const dotsContainer = document.querySelector(".dots");
 
-//Modal
+// Fake user seeding data
+const account1 = {
+  owner: "Michael Beamer",
+  username: "MB",
+  password: "1234",
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-07-26T17:01:17.194Z",
+    "2020-07-28T23:36:17.929Z",
+    "2020-08-01T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "de-DE",
+};
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnShowModal = document.querySelector(".show-modal");
-const btnCloseModal = document.querySelector(".close-modal");
+const account2 = {
+  owner: "Jessica Alba",
+  username: "JA",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 
-function openModal() {
-  overlay.classList.remove("hidden");
-  modal.classList.remove("hidden");
-  modal.style.display = "block";
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
+};
+
+const accounts = [account1, account2];
+
+// Modal component
+
+function modal() {
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const btnShowModal = document.querySelector(".show-modal");
+  const btnCloseModal = document.querySelector(".close-modal");
+
+  function openModal() {
+    overlay.classList.remove("hidden");
+    modal.classList.remove("hidden");
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    overlay.classList.add("hidden");
+    modal.classList.add("hidden");
+    modal.style.display = "none";
+  }
+
+  function closeModalWithEsc(event) {
+    if (event.key === "Escape" && !modal.classList.contains("hidden"))
+      closeModal();
+  }
+
+  btnShowModal.addEventListener("click", openModal);
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+  document.addEventListener("keydown", closeModalWithEsc);
 }
-
-function closeModal() {
-  overlay.classList.add("hidden");
-  modal.classList.add("hidden");
-  modal.style.display = "none";
-}
-
-btnShowModal.addEventListener("click", openModal);
-btnCloseModal.addEventListener("click", closeModal);
-// window.addEventListener("click", closeModal);
 
 // Tabs component
 
@@ -149,14 +201,44 @@ function sliderComponent() {
   dotsContainer.addEventListener("click", dotsSlideControl);
 }
 
-// Modal component
-
 // Initializes everything
 function init() {
   tabsComponent();
   sliderComponent();
+  modal();
 }
 
 init();
 
-// delete min height from grid project cards - gets weird when over 1444p why ? google it
+// LogIn check and setting currentAcc
+
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const submitFormBtn = document.querySelector(".btn-submit");
+const alert = document.querySelector(".alert");
+
+let currentAcc;
+
+submitFormBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  currentAcc = accounts.find((acc) => acc.username === usernameInput.value);
+  // if (
+  //   accounts.find(
+  //     (acc) =>
+  //       acc.username === usernameInput.value &&
+  //       acc.password === passwordInput.value
+  //   )
+  // ) {
+  //   // window.location.assign("app.html");
+  //   console.log("hi");
+  // } else {
+  //   alert.classList.remove("hidden");
+  //   console.log("incorrect");
+  //   setTimeout(function () {
+  //     alert.classList.add("hidden");
+  //   }, 3000);
+  // }
+});
+
+console.log(currentAcc);
